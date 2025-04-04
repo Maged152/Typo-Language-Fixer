5ea37d29344e83a2b31d982208078173429a4275
Ahmed Maged <mageda9000@gmail.com>
2025-04-05 00:20:54 +0200

    Implement clipboard text processing for language detection and correction

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 
 int main(int argc, char **argv) 
 {
+    const std::wstring text = qlm::ReadClipboardText();
+
+    const qlm::Language lang = qlm::DetectLanguage(text);
+
+    const std::wstring fixed_text = qlm::FixLanguage(text, lang);
+
+    qlm::WriteClipboardText(fixed_text);
 
     return 0;
 }
